be661fa01baaf165bf4343e568efc9e3d520af42
Ivan Vanderbyl <ivanvanderbyl@me.com>
2014-01-04 17:58:12 +1100

    Added tests for the running of Agent

---
+++ command/agent/command_test.go
@@ -0,0 +1,54 @@
+package agent
+
+import (
+	"github.com/hashicorp/serf/testutil"
+	"github.com/mitchellh/cli"
+	"testing"
+	"time"
+)
+
+func TestCommand_implements(t *testing.T) {
+	var _ cli.Command = new(Command)
+}
+
+func TestCommandRun(t *testing.T) {
+	shutdownCh := make(chan struct{})
+	defer close(shutdownCh)
+
+	ui := new(cli.MockUi)
+	c := &Command{
+		ShutdownCh: shutdownCh,
+		Ui:         ui,
+	}
+
+	args := []string{
+		"-bind", testutil.GetBindAddr().String(),
+		"-rpc-addr", getRPCAddr(),
+	}
+
+	resultCh := make(chan int)
+	go func() {
+		resultCh <- c.Run(args)
+	}()
+
+	testutil.Yield()
+
+	// Verify it runs "forever"
+	select {
+	case <-resultCh:
+		t.Fatalf("ended too soon, err: %s", ui.ErrorWriter.String())
+	case <-time.After(50 * time.Millisecond):
+	}
+
+	// Send a shutdown request
+	shutdownCh <- struct{}{}
+
+	select {
+	case code := <-resultCh:
+		if code != 0 {
+			t.Fatalf("bad code: %d", code)
+		}
+	case <-time.After(50 * time.Millisecond):
+		t.Fatalf("timeout")
+	}
+}
